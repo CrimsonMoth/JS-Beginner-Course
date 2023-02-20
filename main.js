067664bf7022414c6bde76c9534192bd9e9f4c9c
Alexander <110611277+CrimsonMoth@users.noreply.github.com>
2023-02-20 15:47:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// console.log("Hello from JS");
+
+let userName = "Alex";
+console.log(userName);
+
+// let - переменная
+// Const - константа (неизменяемая)
+// var - старый стандарт
+
+userName = "Igor";
+console.log(userName);
+
+const yOB = 1974;
+const p = "3.14";
