b61b82bca9d4ee57ffa8df00d3fa9118b25dd9a4
Alexander <110611277+CrimsonMoth@users.noreply.github.com>
2023-02-21 14:34:36 +0300

    Add files via upload

--- js/03. types.js
+++ js/03. types.js
@@ -7,3 +7,38 @@
 // someVariable = true; // someVariable становится типа Boolean
 
 // console.log(someVariable);
+// ---------------------------------
+// Значение null «ничего», «пусто» или «значение неизвестно».
+// let age = 32;
+// age = null;
+//  Чтобы сбросить значение переменной
+
+// let someName = "john";
+// someName = null;
+// console.log(someName);
+
+// -----------------------------------
+
+// Значение undefined
+// Означает что «значение не было присвоено
+// Если переменная объявлена, но ей не присвоено никакого значения,
+// то её значением будет undefined
+
+// let userName;
+// console.log(userName);
+// // let newVariable; // undefined
+
+// let someName = "john";
+// someName = null;
+// console.log(someName);
+
+// -------------------------
+
+// Оператор typeof
+
+// const someName = 55;
+// console.log(typeof someName);
+
+// просто показывает тип значения (строка, число, логический тип)
+
+// ---------------------------
