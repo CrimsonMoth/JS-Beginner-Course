593c92340b975575ff5cd601b47f2da12354d058
Alexander <110611277+CrimsonMoth@users.noreply.github.com>
2023-02-21 14:09:09 +0300

    Add files via upload

---
+++ js/03. types.js
@@ -0,0 +1,9 @@
+// let userName = 'Марк'; // Строка (Текст) (Одно слово, фраза, предложение, абзац, хоть целая книга)
+// let age = 30; // Число
+// let isMarried = false; // Логическое значение true/false
+
+// let someVariable = "Some value"; // сейчас someVariable типа Number
+// someVariable = 50; // а теперь someVariable типа String
+// someVariable = true; // someVariable становится типа Boolean
+
+// console.log(someVariable);
--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 // console.log("Hello from JS");
 
-let userName = "Alex";
-console.log(userName);
+// let userName = "Alex";
+// console.log(userName);
 
 // let - переменная
 // Const - константа (неизменяемая)
 // var - старый стандарт
 
-userName = "Igor";
-console.log(userName);
+// userName = "Igor";
+// console.log(userName);
 
-const yOB = 1974;
-const p = "3.14";
+// const yOB = 1974;
+// const p = "3.14";
+
+// function sum() {
+//   var name = "John";
+//   console.log(name);
+// }
+
+// sum();
+
+// console.log(name);
