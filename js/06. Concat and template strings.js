4f8b61967fc8629beec2fcb282b2fb74b06e7b7a
Alexander <110611277+CrimsonMoth@users.noreply.github.com>
2023-02-26 02:27:51 +0300

    Add files via upload

---
+++ js/06. Concat and template strings.js
@@ -0,0 +1,16 @@
+// Конкатенация строк и шаблонные строки
+
+// let greeting = "Hello John";
+// let HAY = " How're you";
+
+// let sayHi = greeting + HAY;
+
+// console.log(sayHi);
+
+// конкатенация 'строка' + 'строка'
+
+const userName = "James";
+console.log("Hello, " + userName + "! How are you?");
+
+// Шаблонные строки и интерполяция
+console.log(`Hello, ${userName}! How are you`);
