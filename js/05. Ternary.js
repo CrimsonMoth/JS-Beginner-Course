7e7326dbae7eb96f066b95f084045948cfaba050
Alexander <110611277+CrimsonMoth@users.noreply.github.com>
2023-02-26 02:10:10 +0300

    Add files via upload

---
+++ js/05. Ternary.js
@@ -0,0 +1,26 @@
+// тернарный оператор
+
+// if (10 < 12) {
+//   console.log("Условие верно");
+// } else {
+//   console.log("Условие НЕ верно");
+// }
+
+// 10 > 12 ? console.log("условие верно") : console.log("условие НЕ верно");
+
+// let greeting;
+// let time = 11;
+
+// if (time < 12) {
+//   greeting = "good morning";
+// } else {
+//   greeting = "good afternoon";
+// }
+
+// console.log(greeting);
+
+// ---------------------------
+
+let time = 21;
+let greeting = time < 21 ? "Good morning" : "Good afternoon";
+console.log(greeting);
