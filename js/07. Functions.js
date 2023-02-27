e02d18021484af3af9f03ff75496e75390b39c7e
Alexander <110611277+CrimsonMoth@users.noreply.github.com>
2023-02-27 17:30:15 +0300

    Add files via upload

---
+++ js/07. Functions.js
@@ -0,0 +1,26 @@
+// =================== 6. Функции ===================
+
+// Описание и вызов функции
+// console.log("Hello User");
+
+// function sayHi() {
+//   console.log("Hello pinky User");
+// }
+
+// sayHi();
+// sayHi();
+// sayHi();
+// sayHi();
+// sayHi();
+
+//function declaration
+
+// function sayHi() {
+//   alert("Hello");
+// }
+
+//function expression
+
+let sayHi = function () {
+  alert("Hey you");
+};
