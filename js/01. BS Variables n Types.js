65d7b0973325dc4475dbb74fbb3f84a60d0252dd
Alexander <110611277+CrimsonMoth@users.noreply.github.com>
2023-03-01 16:34:09 +0300

    Add files via upload

---
+++ js/01. BS Variables n Types.js
@@ -0,0 +1,30 @@
+let a = 10;
+a = 20;
+console.log(a);
+
+let b;
+
+b = false;
+
+console.log(b);
+
+// =========================
+
+const c = 10;
+
+// c = 20;
+
+console.log(c);
+
+// ===========================
+
+// let myName;
+// console.log(myName);
+// myName = "Alex";
+// console.log(myName);
+
+// ===========================
+
+const myName = "Alex";
+
+console.log(myName);
